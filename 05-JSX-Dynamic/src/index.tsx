1c6951787adab9de0f9f72d5f7960207b85ec541
Eric Schmucker <eric.schmucker@blazemetrics.com>
2021-02-27 23:47:17 -0700

    Finish 05-JSX-Dynamic

--- 05-JSX-Dynamic/src/index.tsx
+++ 05-JSX-Dynamic/src/index.tsx
@@ -8,33 +8,98 @@ let tableData = [
   { descr: "Amps", values: [3.5, 3.6, 3.4, 3.3, 3.3, 3.2, 3.1, 2.9] }
 ];
 
+type RowType = { descr: string, values: number[] }
+
 type OurControlArgsType = {
   timestamps: string[];
-  tableData: { descr: string, values: number[] }[];
+  tableData: RowType[];
 }
+
+// const OurControl: Component<OurControlArgsType> = args => (
+//   <div class="ctrl1_container">
+//     <div class="ctrl1_titlebar">
+//       <div class="ctrl1_timestamp">hh:mm:ss</div>
+//       <For each={args.timestamps}>
+//         {ts => <div class="ctrl1_timestamp">{ts}</div>}
+//       </For>
+//     </div>
+//     <div class="ctrl1_datatable">
+//       <For each={args.tableData}>
+//         {row => <OurRowControl row={row} />}
+//       </For>
+//     </div>
+//   </div>
+// );
 
 const OurControl: Component<OurControlArgsType> = args => (
   <div class="ctrl1_container">
-    <div class="ctrl1_titlebar">
-      <div class="ctrl1_timestamp">hh:mm:ss</div>
-      <For each={args.timestamps}>
-        {ts => <div class="ctrl1_timestamp">{ts}</div>}
-      </For>
-    </div>
+    <OurTitleBarControl timestamps={args.timestamps} />
     <div class="ctrl1_datatable">
       <For each={args.tableData}>
-        {row => (
-          <>
-            <div class="ctrl1_descr">{row.descr}</div>
-            <For each={row.values}>
-              {v => <div class="ctrl1_value">{v}</div>}
-            </For>
-          </>
-        )}
+        {row => <OurRowControl row={row} />}
       </For>
     </div>
   </div>
+);
+
+type OurTitleBarControlArgsType = { timestamps: string[] }
+
+const OurTitleBarControl: Component<OurTitleBarControlArgsType> = args => (
+  <div class="ctrl1_titlebar">
+    <div class="ctrl1_timestamp">hh:mm:ss</div>
+    <For each={args.timestamps}>
+      {ts => <div class="ctrl1_timestamp">{ts}</div>}
+    </For>
+  </div>
 )
+
+type OurRowControlArgsType = {
+  row: RowType;
+}
+
+const OurRowControl: Component<OurRowControlArgsType> = args => (
+  <>
+    <div class="ctrl1_descr">{args.row.descr}</div>
+    <For each={args.row.values}>
+      {v => <div class="ctrl1_value">{v}</div>}
+    </For>
+  </>
+);
+
+// const OurRowControl: Component<OurRowControlArgsType> = args => {
+//   console.log(args.row.descr);
+//   return (
+//   <>
+//     <div class="ctrl1_descr">{args.row.descr}</div>
+//     <For each={args.row.values}>
+//       {v => <div class="ctrl1_value">{v}</div>}
+//     </For>
+//   </>
+//   );
+// }
+
+// const OurControl: Component<OurControlArgsType> = args => (
+//   <div class="ctrl1_container">
+//     <div class="ctrl1_titlebar">
+//       <div class="ctrl1_timestamp">hh:mm:ss</div>
+//       <For each={args.timestamps}>
+//         {ts => <div class="ctrl1_timestamp">{ts}</div>}
+//       </For>
+//     </div>
+//     <div class="ctrl1_datatable">
+//       <For each={args.tableData}>
+//         {row => (
+//           <>
+//             <div class="ctrl1_descr">{row.descr}</div>
+//             <For each={row.values}>
+//               {v => <div class="ctrl1_value">{v}</div>}
+//             </For>
+//           </>
+//         )}
+//       </For>
+//     </div>
+//   </div>
+// )
 
 if (window.addEventListener) {
   window.addEventListener("load", () => {
